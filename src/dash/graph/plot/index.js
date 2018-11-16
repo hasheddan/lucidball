567cbc558dd9d9a1b1b47ebddfa0bc4b29bfeab5
HashedDan <georgedanielmangum@gmail.com>
2018-11-16 13:18:11 -0600

    finding data points

--- src/dash/graph/plot/index.js
+++ src/dash/graph/plot/index.js
@@ -49,14 +49,16 @@ export default class Plot extends React.Component {
         canvas.setAttribute('width', style_width * dpi);
         console.log("Height: " + canvas.height)
         console.log("Width: " + canvas.width)
+        // Make on half pixels for sharper render
         ctx.translate(0.5,0.5)
+        // Clear current canvas
         ctx.clearRect(0,0, canvas.width, canvas.height);
+        // Determine x and y coordinates on canvas
         var clientRect = canvas.getBoundingClientRect(), // abs. size of element
             scaleX = canvas.width / clientRect.width,    // relationship bitmap vs. element for X
-            scaleY = canvas.height / clientRect.height; 
-        canvas.onmousemove = (e) => {
-            console.log("X: " + (e.clientX - clientRect.left)*scaleX+ ", Y: " + (e.clientY - clientRect.top)*scaleY)
-        }
+            scaleY = canvas.height / clientRect.height;
+        // Get Plot bounds
+        var minX = 40, maxX = canvas.width-20, minY = canvas.height-70, maxY = 20
         // draw children “components”
         ctx.lineWidth=2;
         ctx.strokeStyle="rgba(192,192,192,0.7)";
@@ -65,21 +67,45 @@ export default class Plot extends React.Component {
         console.log("INTERVALS: " + (canvas.width))
         var interval = intervals + 40
         ctx.fillStyle="blue"
+        // Array of interval x positions
+        var inters = []
         for (var i = 0; i < this.props.data.length-1; i++) {
+            inters.push(interval)
             line({ctx, startx: interval, starty: 20, endx: interval, endy: canvas.height-70});
             var pointY = plotY(canvas.height, 30, 0, this.props.data[i][24])
-            console.log("Point: " + pointY + ", Points: " + this.props.data[i][24])
+            console.log("Point: " + pointY + ", Points: " + this.props.data[i][24] + ", X: " + interval)
             rect({ctx, x: interval-10, y: pointY-10, width: 20, height: 20})
             ctx.strokeStyle="rgba(192,192,192,0.7)";
             interval += intervals
         }
+        // Draw plot axis
         ctx.lineWidth=4;
         ctx.strokeStyle="black";
         ctx.fillStyle="black"
-        line({ctx, startx: 40, starty: canvas.height-70, endx: canvas.width-20, endy: canvas.height-70});
-        line({ctx, startx: 40, starty: canvas.height-70, endx: 40, endy: 20});
+        line({ctx, startx: minX, starty: minY, endx: maxX, endy: minY});
+        line({ctx, startx: minX, starty: minY, endx: minX, endy: maxY});
         ctx.font = '30px Allerta Stencil';
         ctx.fillText(this.props.player + ' - '+ this.props.team, 40, canvas.height-15)
+
+        // Track mouse
+        canvas.onmousemove = (e) => {
+            this.updateCanvas()
+            var xPos = (e.clientX - clientRect.left)*scaleX
+            var yPos = (e.clientY - clientRect.top)*scaleY
+            // console.log("X: " + xPos + "{xMax: "+maxX+",xMin: "+minX+ "}, Y: " + yPos)
+            if ((xPos > minX && xPos < maxX) && (yPos > maxY && yPos < minY)) {
+                // ctx.restore()
+                // ctx.save()
+                console.log("X: " + xPos + ", Y: " + yPos)
+                for (i in inters) {
+                    if (xPos > inters[i] - 10 && xPos < inters[i] + 10) {
+                        console.log("~~~~~~~AH~~~~~~~")
+                        alert("DATA POINT!")
+                    }
+                }
+                line({ctx, startx: xPos, starty: 20, endx: xPos, endy: canvas.height-70});
+            }
+        }
     }
 
     render() {
