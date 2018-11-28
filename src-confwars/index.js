12275d6837194ca683a13088351ee8c389317a78
HashedDan <georgedanielmangum@gmail.com>
2018-11-27 18:55:54 -0600

    updated landing and error pages

--- src-confwars/index.js
+++ src-confwars/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Plot from './plot'
 
 class App extends React.Component {
     constructor(props) {
@@ -12,26 +13,27 @@ class App extends React.Component {
             <div className="container-fluid" style={{ height: "100vh" }}>
                 <div className="row" style={{backgroundColor: "#FFFAFA", height: "38%", borderBottom: "2px solid black", zIndex: "999", position: "relative"}}>
                     <div className="col-5" style={{padding: "0 0 0 0", height: "100%", borderRight: "2px solid black", textAlign: "center"}}>
-                        <h1>West</h1>
+                        <h1 style={{textDecoration: "underline"}}>West</h1>
                         <div style={{fontSize: "20vh"}}>35</div>
                     </div>
                     <div className="col-2" style={{padding: "0 0 0 0", height: "100%", textAlign: "center"}}>
-                        <h1>Date</h1>
-                        <div style={{fontSize: "3vh"}}>October 13, 2018</div>
+                        <h1 style={{textDecoration: "underline"}}>Date</h1>
+                        <div style={{fontSize: "3vh", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>October 13, 2018</div>
                     </div>
                     <div className="col-5" style={{padding: "0 0 0 0", height: "100%", borderLeft: "2px solid black", textAlign: "center"}}>
-                        <h1>East</h1>
+                        <h1 style={{textDecoration: "underline"}}>East</h1>
                         <div style={{fontSize: "20vh"}}>35</div>
                     </div>
                 </div>
                 <div className="row" style={{backgroundColor: "white", height: "56%", zIndex: "9999", position: "relative"}}>
+                    <Plot />
                 </div>
                 <div className="row" style={{backgroundColor: "#FFFAFA", height: "6%", borderTop: "2px solid black", zIndex: "999", position: "relative"}}>
-                    <div style={{ fontSize: "4vh", paddingLeft: "1vw" }}>Thank you for supporting our work.</div>
+                    <div style={{ fontSize: "4vh", paddingLeft: "1vw", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>Thank you for supporting our work.</div>
                     <a className="btn" href="https://twitter.com/intent/tweet?text=" data-url="https://dev.twitter.com/web/tweet-button"><i className="fab fa-twitter"></i></a>
                     <a className="btn" href="https://www.paypal.me/lucidball" style={{color: "#003087"}}><i className="fab fa-paypal"></i></a>
                     <a className="btn" href="https://github.com/HashedDan/lucidball" style={{color: "black"}}><i className="fab fa-github"></i></a>
-                    <h3 href="https://twitter.com/lucidball" style={{ position: "absolute", right: "10px", fontFamily: "Permanent Marker", color: "red", fontSize: "4vh"}}>LucidBall</h3>
+                    <h3 style={{ position: "absolute", right: "10px", fontFamily: "Permanent Marker", color: "red", fontSize: "4vh", cursor: "pointer"}}><a href="https://lucidball.com" style={{textDecoration: "none", color: "red"}} >LucidBall</a></h3>
                 </div>
             </div>
         );
--- src-confwars/plot/index.js
+++ src-confwars/plot/index.js
@@ -55,77 +55,77 @@ export default class Plot extends React.Component {
         // Clear current canvas
         ctx.clearRect(0,0, canvas.width, canvas.height)
         // Draw Background
-        ctx.fillStyle = "#DCDCDC"
+        ctx.fillStyle = "#FFFAFA"
         ctx.fillRect(0, 0, canvas.width, canvas.height)
         // Determine x and y coordinates on canvas
         var clientRect = canvas.getBoundingClientRect(), // abs. size of element
             scaleX = canvas.width / clientRect.width,    // relationship bitmap vs. element for X
             scaleY = canvas.height / clientRect.height;
         // Get Plot bounds
-        var minX = 60, maxX = canvas.width-20, minY = canvas.height-70, maxY = 20
+        var minX = 60, maxX = canvas.width-20, minY = canvas.height-20, maxY = 20
         // draw children “components”
         ctx.lineWidth=2;
         ctx.strokeStyle="rgba(192,192,192,0.7)";
         ctx.lineCap="round";
-        const intervals = (canvas.width-40) / this.props.data.length
-        console.log("INTERVALS: " + (canvas.width))
-        var interval = 70
-        ctx.fillStyle="blue"
-        // Get max & min point value
-        var maxPoints = 0
-        var minPoints = 0
-        for (var i = 0; i < this.props.data.length-1; i++) {
-            // Get max value
-            this.props.data[i][this.props.stat] > maxPoints && (maxPoints = this.props.data[i][this.props.stat])
-            // If min value is less than 0, set to that value
-            if (this.props.data[i][this.props.stat] < 0) {
-                minPoints = this.props.data[i][this.props.stat]
-            }
-        }
-        // Scale y axis maximum to be 20% more than max value
-        maxPoints = (.2 * maxPoints) + maxPoints
-        // Array of interval x positions
-        var inters = []
-        var avgs = []
-        var prevY = minY
-        var prevX = minX
-        var total = 0
-        var gameCount = 0
-        for (var i = this.props.data.length-1; i >= 0; i--) {
-            inters.push(interval)
-            total += this.props.data[i][this.props.stat]
-            gameCount++
-            console.log("GAME COUNT: "+gameCount)
-            line({ctx, startx: interval, starty: 20, endx: interval, endy: canvas.height-70});
-            var pointY = plotY(canvas.height, maxPoints, minPoints, this.props.data[i][this.props.stat])
-            console.log("Point: " + pointY + ", Points: " + this.props.data[i][this.props.stat] + ", X: " + interval)
-            rect({ctx, x: interval-10, y: pointY-10, width: 20, height: 20})
-            ctx.strokeStyle="black";
-            line({ctx, startx: prevX, starty: prevY, endx: interval, endy: pointY});
-            ctx.strokeStyle="rgba(192,192,192,0.7)";
-            // Plot Average
-            ctx.fillStyle="rgba(255,0,0,.4)"
-            var curAvg = total / gameCount
-            console.log("AVERAGE: " + curAvg)
-            avgs.push(curAvg)
-            var plotAvg = plotY(canvas.height, maxPoints, minPoints, curAvg)
-            rect({ctx, x: interval-10, y: plotAvg-10, width: 20, height: 20})
-            ctx.fillStyle="blue"
-            // Update Pos
-            prevY = pointY
-            prevX = interval
-            interval += intervals
-        }
+        // const intervals = (canvas.width-40) / this.props.data.length
+        // console.log("INTERVALS: " + (canvas.width))
+        // var interval = 70
+        // ctx.fillStyle="blue"
+        // // Get max & min point value
+        // var maxPoints = 0
+        // var minPoints = 0
+        // for (var i = 0; i < this.props.data.length-1; i++) {
+        //     // Get max value
+        //     this.props.data[i][this.props.stat] > maxPoints && (maxPoints = this.props.data[i][this.props.stat])
+        //     // If min value is less than 0, set to that value
+        //     if (this.props.data[i][this.props.stat] < 0) {
+        //         minPoints = this.props.data[i][this.props.stat]
+        //     }
+        // }
+        // // Scale y axis maximum to be 20% more than max value
+        // maxPoints = (.2 * maxPoints) + maxPoints
+        // // Array of interval x positions
+        // var inters = []
+        // var avgs = []
+        // var prevY = minY
+        // var prevX = minX
+        // var total = 0
+        // var gameCount = 0
+        // for (var i = this.props.data.length-1; i >= 0; i--) {
+        //     inters.push(interval)
+        //     total += this.props.data[i][this.props.stat]
+        //     gameCount++
+        //     console.log("GAME COUNT: "+gameCount)
+        //     line({ctx, startx: interval, starty: 20, endx: interval, endy: canvas.height-70});
+        //     var pointY = plotY(canvas.height, maxPoints, minPoints, this.props.data[i][this.props.stat])
+        //     console.log("Point: " + pointY + ", Points: " + this.props.data[i][this.props.stat] + ", X: " + interval)
+        //     rect({ctx, x: interval-10, y: pointY-10, width: 20, height: 20})
+        //     ctx.strokeStyle="black";
+        //     line({ctx, startx: prevX, starty: prevY, endx: interval, endy: pointY});
+        //     ctx.strokeStyle="rgba(192,192,192,0.7)";
+        //     // Plot Average
+        //     ctx.fillStyle="rgba(255,0,0,.4)"
+        //     var curAvg = total / gameCount
+        //     console.log("AVERAGE: " + curAvg)
+        //     avgs.push(curAvg)
+        //     var plotAvg = plotY(canvas.height, maxPoints, minPoints, curAvg)
+        //     rect({ctx, x: interval-10, y: plotAvg-10, width: 20, height: 20})
+        //     ctx.fillStyle="blue"
+        //     // Update Pos
+        //     prevY = pointY
+        //     prevX = interval
+        //     interval += intervals
+        // }
         // Draw plot axis
-        ctx.lineWidth=4;
+        ctx.lineWidth=6;
         ctx.strokeStyle="black";
         ctx.fillStyle="black"
-        line({ctx, startx: minX, starty: minY, endx: maxX, endy: minY});
+        line({ctx, startx: minX, starty: .5*minY, endx: maxX, endy: .5*minY});
         line({ctx, startx: minX, starty: minY, endx: minX, endy: maxY});
         // line({ctx, startx: minX, starty: minY-50, endx: minX-10, endy: minY-50});
         ctx.font = '30px Inconsolata';
-        ctx.fillText(this.props.player + ' - '+ this.props.team, 60, canvas.height-25)
-        ctx.fillText('LucidBall', canvas.width-200, canvas.height-25)
+        // ctx.fillText(this.props.player + ' - '+ this.props.team, 60, canvas.height-25)
+        // ctx.fillText('LucidBall', canvas.width-200, canvas.height-25)
 
         // Track mouse
         canvas.onmousemove = (e) => {
@@ -138,26 +138,26 @@ export default class Plot extends React.Component {
                 // ctx.save()
                 console.log("X: " + xPos + ", Y: " + yPos)
                 var hover = false
-                for (i in inters) {
-                    if (xPos > inters[i] - 10 && xPos < inters[i] + 10) {
-                        console.log("~~~~~~~AH~~~~~~~")
-                        ctx.strokeStyle = "yellow"
-                        line({ctx, startx: xPos, starty: 20, endx: xPos, endy: canvas.height-70})
-                        ctx.strokeStyle = "black"
-                        ctx.fillStyle = "white"
-                        ctx.fillRect(minX, minY-100, 400, 100)                        
-                        ctx.strokeRect(minX, minY-100, 400, 100)
-                        ctx.fillStyle = "black"
-                        ctx.font = '27px Inconsolata'
-                        ctx.fillText('Game: ' + this.props.data[this.props.data.length-1-i][4], minX+60, minY-70)
-                        ctx.fillText((boxStats.find(stat => stat.Index == this.props.stat)).Stat+ ': ' + this.props.data[this.props.data.length-1-i][this.props.stat], minX+80, minY-40)
-                        ctx.fillText('Average: ' + Math.round(avgs[i] * 100) / 100, minX+70, minY-10)
-                        hover = true
-                    }
-                }
+                // for (i in inters) {
+                //     if (xPos > inters[i] - 10 && xPos < inters[i] + 10) {
+                //         console.log("~~~~~~~AH~~~~~~~")
+                //         ctx.strokeStyle = "yellow"
+                //         line({ctx, startx: xPos, starty: 20, endx: xPos, endy: canvas.height-70})
+                //         ctx.strokeStyle = "black"
+                //         ctx.fillStyle = "white"
+                //         ctx.fillRect(minX, minY-100, 400, 100)                        
+                //         ctx.strokeRect(minX, minY-100, 400, 100)
+                //         ctx.fillStyle = "black"
+                //         ctx.font = '27px Inconsolata'
+                //         ctx.fillText('Game: ' + this.props.data[this.props.data.length-1-i][4], minX+60, minY-70)
+                //         ctx.fillText((boxStats.find(stat => stat.Index == this.props.stat)).Stat+ ': ' + this.props.data[this.props.data.length-1-i][this.props.stat], minX+80, minY-40)
+                //         ctx.fillText('Average: ' + Math.round(avgs[i] * 100) / 100, minX+70, minY-10)
+                //         hover = true
+                //     }
+                // }
                 if (!hover) {
                     ctx.strokeStyle="red"
-                    line({ctx, startx: xPos, starty: 20, endx: xPos, endy: canvas.height-70})
+                    line({ctx, startx: xPos, starty: 20, endx: xPos, endy: canvas.height-20})
                 }
             }
         }
@@ -176,8 +176,8 @@ export default class Plot extends React.Component {
 
     render() {
         return(
-            <div style={{ height: "100%", width: "100%", position: "relative"}}>
-                <div style={{ height: "95%", width: "95%", borderRadius: "10px", backgroundColor: "#DCDCDC", top: "52%", left: "50%", transform: "translate(-50%, -50%)", position: "absolute", fontFamily: "Allerta Stencil"}}>
+            <div style={{ height: "100%", width: "100%", position: "relative", backgroundColor: "#FFFAFA"}}>
+                <div style={{ height: "90%", width: "95%", borderRadius: "10px", backgroundColor: "#FFFAFA", top: "50%", left: "50%", transform: "translate(-50%, -50%)", position: "absolute", fontFamily: "Allerta Stencil"}}>
                     <canvas ref="canvas" style={{height: "100%", width: "100%"}}/>
                     <img  src={this.state.src}  style={{ display: "none" }}/>
                 </div>
